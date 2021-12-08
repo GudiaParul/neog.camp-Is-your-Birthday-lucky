9b0ee1d381c184345056890d3fad62ab9d592c10
Parul Gupta <guptaparul1002@gmail.com>
2021-12-08 14:31:31 +0530

    middle commit

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 var dob = document.querySelector("#dob");
 var luckyNumber = document.querySelector("#luckyNumber");
 var checkButton = document.querySelector("#checkButton");
-var outputBox = document.querySelector("#outputBox");
+var output = document.querySelector("#output");
 
 function clickEventHandler() {
+   //hideMessage();
     var birthDate = dob.value
+    var lc = luckyNumber.value
     var cSum = calculateSum(birthDate)
-    compareValues(cSum,luckyNumber.value)
+    compareValues(cSum , lc)
 }
 
 function calculateSum(birthDate) {
+    //hideMessage();
     var birthDate = birthDate.replaceAll("-", "")
     var sum = 0;
     for (var i=0 ; i<birthDate.length ; i++) {
-    sum = sum + Number(birthDate(i));
+    sum = sum + Number(birthDate.charAt[i]);
 }
 return sum;
 }
 
-function compareValues(cSum,luckyNumber) {
-    if (cSum % luckyNumber === 0){
-    outputBox.innerText="you are lucky";
+function compareValues(cSum,lc) {
+    hideMessage();
+    if (cSum % lc === 0){
+    output.innerText="you are lucky";
 } else {
-    outputBox.InnerText="you are unlucky";
+    output.nnerText="you are unlucky";
 }
 }
+
+function hideMessage() {
+    message.style.display = "none";
+  }
+  
+
+
 checkButton.addEventListener("click",clickEventHandler);
